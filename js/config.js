2c35ef7f6c08362dea746962980a29a8ff6fa86b
No, i want to stay anonymous <34982029+PeRSHH5Y98aF1JhpgDCL@users.noreply.github.com>
2020-03-12 17:53:15 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -27,14 +27,23 @@ let config = {
 	  HyperExpoCSI: D(2),
       desc: 'Dimension powers are raised to the power of 1.2 but only if greater than 1 for each upgrade, also wipes other upgrades, uncapped.',
       levelCap: D(1e308),
-      onBuy: [()=>{game.upgrades.dimComp.level.array=[[0,0]];game.upgrades.dimStab.level.array=[[0,0]];},"resetLayer",'[{"array":[[0,0]],"layer":0,"sign":1}]']
+      onBuy: [()=>{game.upgrades.tickspeed.level.array=[[0,0]];game.upgrades.dimStab.level.array=[[0,0]];},"resetLayer",'[{"array":[[0,0]],"layer":0,"sign":1}]']
     },
 	  tickspeed:{
 	  	baseCost:D(1e3),
 		costScale:D(1e1),
 		HyperExpoCSI:D(1.01),
 		name:"Tickspeed",
-		desc:'Multiplies Dimensions by 1.1',
+		desc:'Multiplies Dimensions by 1.1, uncapped.(affects after dimColl)',
+		levelCap:D(1e308)
+	  },
+	  dimSale:{
+	  	baseCost:D(1e15),
+		costScale:D(1e15),
+		CSI:D(1e10),
+		HyperExpoCSI:D(1.01),
+		name:"Dimensional Sale",
+		desc:'Dimension costs are reduced by ^0.8, uncapped.',
 		levelCap:D(1e308)
 	  }
   }
