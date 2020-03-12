f1d7fa9a7740f722b00bb52e140b268a731dc4b9
No, i want to stay anonymous <34982029+PeRSHH5Y98aF1JhpgDCL@users.noreply.github.com>
2020-03-12 13:34:01 +0800

    Update ExpantaNum.js

--- js/lib/ExpantaNum.js
+++ js/lib/ExpantaNum.js
@@ -154,7 +154,6 @@
   Q.negate=Q.neg=function (x){
     return new ExpantaNum(x).neg();
   };
-  P.self=this
   P.compareTo=P.cmp=function (other){
     other=new ExpantaNum(other);
     if (isNaN(this.array[0][1])||isNaN(other.array[0][1])) return NaN;
@@ -338,6 +337,12 @@
     p=q=null;
     return t;
   };
+  P.addBy=function (x) {
+    let y=P.add(x)
+    this.array=y.array
+    this.sign=y.sign
+    this.layer=y.layer
+  }
   Q.plus=Q.add=function (x,y){
     return new ExpantaNum(x).add(y);
   };
